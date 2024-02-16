c85a8965d0a80e6efc36f504c8313689bae71fcd
sidbalito <sidbalito@gmail.com>
2024-02-15 22:34:22 -0300

    Embaralha os botões, oculta o botão após a jogada. Incui modo robô.

--- jokenpo/index.js
+++ jokenpo/index.js
@@ -1,18 +1,45 @@
 let p1, p2;
 let redefinido = false;
+let modoRobô1, modoRobô2
+const OPÇÕES = ['pedra', 'papel', 'tesoura']
 redefinir()
 
 const valores = {
     "pedra x pedra": "Empate",
     "papel x papel": "Empate",
     "tesoura x tesoura": "Empate",
-    "pedra x papel": "p2 win",
-    "pedra x tesoura": "p1 win",
-    "papel x pedra": "p1 win",
-    "papel x tesoura": "p2 win",
-    "tesoura x pedra": "p2 win",
-    "tesoura x papel": "p1 win",
+    "pedra x papel": "Jogador 2 venceu",
+    "pedra x tesoura": "Jogador 1 venceu",
+    "papel x pedra": "Jogador 1 venceu",
+    "papel x tesoura": "Jogador 2 venceu",
+    "tesoura x pedra": "Jogador 2 venceu",
+    "tesoura x papel": "Jogador 1 venceu",
 };
+
+function escolha(jogador, escolha){
+    let elemento = null
+    switch(jogador){
+        case 'p1':
+            elemento = escolha1
+            p1 = escolha
+            botões1.style.display = 'none'
+            botões2.style.display = 'block'
+            escolha1.innerText = ''
+            escolha2.innerText = 'Aguardando...'
+            if(p2) display()
+            if(modoRobô2)
+                setTimeout(jogadaRobô, 3000, 'p2')
+            break
+        case 'p2':
+            elemento = escolha2
+            p2 = escolha
+            escolha2.innerText = ''
+            botões2.style.display = 'none'
+            if(p1) display()
+            break
+    }
+    //elemento.innerText =  'escolheu'
+}
 
 function escolhaP1(escolha){
     if(redefinido){
@@ -34,6 +61,8 @@ function escolhaP2(escolha){
 
 function display(){
     redefinido = false;
+    escolha1.innerText = p1
+    escolha2.innerText = p2
     var resultado = `${p1} x ${p2}`;
     document.getElementById('display').innerText =  valores[resultado];
 }
@@ -41,8 +70,86 @@ function display(){
 function redefinir(){
     p1 = undefined;
     p2 = undefined;
+    funçãoModoRobô(humano1)
+    funçãoModoRobô(humano2)
+    funçãoModoRobô(robô1)
+    funçãoModoRobô(robô2)
+    exibeModoRobô(humano1, robô1)
+    exibeModoRobô(humano2, robô2)
     document.getElementById('display').innerText = 'Façam suas escolhas'
-    document.getElementById('escolha1').innerText = ''
+    document.getElementById('escolha1').innerText = 'Aguardando...'
     document.getElementById('escolha2').innerText = ''
+    while(botões1.firstChild) botões1.removeChild(botões1.firstChild) 
+    while(botões2.firstChild) botões2.removeChild(botões2.firstChild)
+    botões1.style.display = 'block'
+    botões2.style.display = 'none'
+    embaralha(OPÇÕES).forEach((item)=>adicionaBotão(botões1, 'p1', item))
+    embaralha(OPÇÕES).forEach((item)=>adicionaBotão(botões2, 'p2', item))
+    if(modoRobô1) 
+        setTimeout(jogadaRobô, 3000, 'p1')
     redefinido = true;
 }
+
+function jogadaRobô(jogador){
+    escolha(jogador, OPÇÕES[(Math.random()*(OPÇÕES.length-1)).toFixed(0)])
+}
+
+function funçãoModoRobô(elemento){
+    elemento.onclick = ()=>{
+        switch(elemento){
+            case humano1:
+                alteraModoRobô('p1', false)
+                break
+            case humano2:
+                alteraModoRobô('p2', false)
+                break
+            case robô1:
+                alteraModoRobô('p1', true)
+                break
+            case robô2:
+                alteraModoRobô('p2', true)
+                break
+        }        
+    }
+}
+
+function alteraModoRobô(jogador, flag){
+    switch(jogador){
+        case 'p1':
+            modoRobô1 = flag
+            exibeModoRobô(humano1, robô1, modoRobô1)
+            break
+        case 'p2':
+            modoRobô2 = flag
+            exibeModoRobô(humano2, robô2, modoRobô2)
+            break
+    }
+}
+
+function exibeModoRobô(humano, robô, modo){
+    humano.src = modo ? 'garoto-mono.png' : 'garoto.png'
+    humano.title = modo ? 'Humano (desativado)' : 'Humano'
+    robô.src = modo ? 'robo.png' : 'robo-mono.png'
+    robô.title = modo ? 'Robô' : 'Robô (desativado)'
+}
+
+function adicionaBotão(div, jogador, s){
+    let botão = document.createElement('button')
+    switch(jogador){
+        case 'p1': botão.onclick = ()=>escolha(jogador, s)
+            break
+        case 'p2': botão.onclick = ()=>escolha(jogador, s)
+            break 
+    }
+    botão.innerText = s
+    div.appendChild(botão)
+  }
+
+  function embaralha(original){
+    matriz = [...original]
+    let retorno = []
+    while(matriz.length > 0){
+      retorno.push(matriz.splice(Math.random() * matriz.length, 1))
+    }
+    return retorno
+  }
